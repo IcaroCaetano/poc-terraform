ba2358d1d16e3beac8df58c667eadc02d4f22516
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-09-08 15:09:00 -0700

    Update lambda.py

--- aws-cloudwatch/lambda.py
+++ aws-cloudwatch/lambda.py
@@ -1,14 +1,15 @@
 import json
 import logging
 
+# Configure the logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def handler(event, context):
+    # Log a message to CloudWatch
     logger.info("🚀 Lambda POC executed successfully!")
     logger.debug("🔍 Event received: %s", json.dumps(event))
 
-    # Retorno simples
     return {
         "statusCode": 200,
         "body": json.dumps({"message": "Hello from Lambda POC!"})
