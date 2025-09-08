9171cb67cffbd52bd43c144a766ba2ec8dd3d75d
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-09-08 15:12:06 -0700

    Update index.js

--- aws-cloudwatch/index.js
+++ aws-cloudwatch/index.js
@@ -1,7 +1,7 @@
-# Defines the main Lambda function, called a handler.
-# The handler is the entry point of the Lambda function, that is, what AWS executes when Lambda is invoked.
-# The event parameter contains the input data that triggered the Lambda (e.g., API Gateway call, S3 event, etc.).
-# The function is asynchronous, allowing the use of await for asynchronous operations (such as HTTP calls or database queries).
+// Defines the main Lambda function, called a handler.
+// The handler is the entry point of the Lambda function, that is, what AWS executes when Lambda is invoked.
+// The event parameter contains the input data that triggered the Lambda (e.g., API Gateway call, S3 event, etc.).
+// The function is asynchronous, allowing the use of await for asynchronous operations (such as HTTP calls or database queries).
 exports.handler = async (event) => {
     console.log("Lambda Log Working!");
     return { statusCode: 200, body: "Lambda executed successfully!" };
