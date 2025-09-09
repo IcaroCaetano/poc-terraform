203e96aa5cd5d7216e5dfc4a9776f5a95dcdd600
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-09-09 14:16:05 -0700

    Update provider.tf

--- poc-s3-dynamodb/provider.tf
+++ poc-s3-dynamodb/provider.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3       = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+  }
+}
 
