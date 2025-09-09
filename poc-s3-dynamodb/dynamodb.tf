68ffb6473542fa08c2c098098574e8ddff120894
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-09-09 14:17:40 -0700

    Update dynamodb.tf

--- poc-s3-dynamodb/dynamodb.tf
+++ poc-s3-dynamodb/dynamodb.tf
@@ -1,1 +1,11 @@
+resource "aws_dynamodb_table" "poc_table" {
+  name         = "poc-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
 
