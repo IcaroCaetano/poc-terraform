aa44d34afc857fb63bd6b43544ae84e0a979ac48
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-09-19 10:44:27 -0300

    feat: add output dns

--- poc-terraform-ec2-sg/outputs.tf
+++ poc-terraform-ec2-sg/outputs.tf
@@ -2,3 +2,8 @@ output "ec2_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.ec2_poc.public_ip
 }
+
+output "ec2_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.ec2_poc.public_dns
+}
